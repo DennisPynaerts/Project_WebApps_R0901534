64f66fa665fd991fa8d951718515ef5987d03e9e
DennisPynaerts <96107261+DennisPynaerts@users.noreply.github.com>
2022-10-12 16:33:38 +0200

    Create first 3 models

---
+++ Project_WebApps_R0901534_Solution/Project_WebApps_R0901534_ASP/Models/Merk.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project_WebApps_R0901534_ASP.Models
+{
+    public class Merk
+    {
+        // Primary Key
+
+        public int MerkId { get; set; }
+
+        // Attributes
+
+        [Required(ErrorMessage = "Gelieve een naam in te vullen!")]
+        public string Naam { get; set; }
+    }
+}
---
+++ Project_WebApps_R0901534_Solution/Project_WebApps_R0901534_ASP/Models/Model.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project_WebApps_R0901534_ASP.Models
+{
+    public class Model
+    {
+        public int ModelId { get; set; }
+
+        // Attributes
+
+        [Required(ErrorMessage = "Gelieve een naam in te vullen!")]
+        public string Naam { get; set; }
+    }
+}
