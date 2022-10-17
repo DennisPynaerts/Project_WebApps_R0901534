4d199586f3f892e81492a880c5a79bb29e75e532
DennisPynaerts <96107261+DennisPynaerts@users.noreply.github.com>
2022-10-17 14:44:00 +0200

    Add ViewModels for Circuit

---
+++ Project_WebApps_R0901534_Solution/Project_WebApps_R0901534_ASP/ViewModels/CircuitDetailViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Project_WebApps_R0901534_ASP.ViewModels
+{
+    public class CircuitDetailViewModel
+    {
+        public int CircuitId { get; set; }
+        public string Naam { get; set; }
+        public string Afbeelding { get; set; }
+    }
+}
