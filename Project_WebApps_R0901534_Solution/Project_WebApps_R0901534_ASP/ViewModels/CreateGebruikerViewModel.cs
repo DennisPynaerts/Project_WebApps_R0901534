6f625dd0099dc7c507f335cc7bf5c8904bfb2243
DennisPynaerts <96107261+DennisPynaerts@users.noreply.github.com>
2022-10-17 15:40:42 +0200

    Start Layout Home

---
+++ Project_WebApps_R0901534_Solution/Project_WebApps_R0901534_ASP/ViewModels/CreateGebruikerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project_WebApps_R0901534_ASP.ViewModels
+{
+    public class CreateGebruikerViewModel
+    {
+        public string Nickname { get; set; }
+        public string Emailadres { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Wachtwoord { get; set; }
+    }
+}
