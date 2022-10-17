a6bf1f7d428d9f4d30dfd89b06c98cc4e871b91f
DennisPynaerts <96107261+DennisPynaerts@users.noreply.github.com>
2022-10-17 15:15:42 +0200

    Add ViewModels

---
+++ Project_WebApps_R0901534_Solution/Project_WebApps_R0901534_ASP/ViewModels/GebruikerAutoListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Project_WebApps_R0901534_ASP.Models;
+using System.Collections.Generic;
+
+namespace Project_WebApps_R0901534_ASP.ViewModels
+{
+    public class GebruikerAutoListViewModel
+    {
+        public List<GebruikerAuto> GebruikerAutos { get; set; }
+    }
+}
