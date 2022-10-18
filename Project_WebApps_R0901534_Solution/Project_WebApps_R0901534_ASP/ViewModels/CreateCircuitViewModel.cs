c0e20779b2346bf409cec550c2b5605d68f7acf4
DennisPynaerts <96107261+DennisPynaerts@users.noreply.github.com>
2022-10-18 10:50:27 +0200

    HelloWorld

--- Project_WebApps_R0901534_Solution/Project_WebApps_R0901534_ASP/ViewModels/CreateCircuitViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Project_WebApps_R0901534_ASP.ViewModels
-{
-    public class CreateCircuitViewModel
-    {
-        public int CircuitId { get; set; }
-        public string Naam { get; set; }
-        public string Afbeelding { get; set; }
-    }
-}
