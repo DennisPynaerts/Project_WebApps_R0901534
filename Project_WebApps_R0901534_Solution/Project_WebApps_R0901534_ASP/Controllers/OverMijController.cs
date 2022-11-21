6e880180a65f2fad494f0658b4ea3aaf55a5e687
DennisPynaerts <96107261+DennisPynaerts@users.noreply.github.com>
2022-11-21 09:29:09 +0100

    Add content to OverMijView from db

--- Project_WebApps_R0901534_Solution/Project_WebApps_R0901534_ASP/Controllers/OverMijController.cs
+++ Project_WebApps_R0901534_Solution/Project_WebApps_R0901534_ASP/Controllers/OverMijController.cs
@@ -1,12 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Project_WebApps_R0901534_ASP.Data;
+using Project_WebApps_R0901534_ASP.Models;
+using Project_WebApps_R0901534_ASP.ViewModels;
+using System.Linq;
 
 namespace Project_WebApps_R0901534_ASP.Controllers
 {
     public class OverMijController : Controller
     {
-        public IActionResult Index()
+        private readonly ILogger<LeaderboardController> _logger;
+        private readonly ForzaContext _ctx;
+
+        public OverMijController(ILogger<LeaderboardController> logger, ForzaContext ctx)
         {
-            return View();
+            _logger = logger;
+            _ctx = ctx;
+        }
+
+        public IActionResult Index(int id = 1)
+        {
+            OverMij overMij = _ctx.OverMijs.Where(o => o.OverMijId == id).FirstOrDefault();
+            if (overMij == null)
+                return RedirectToAction("Index");
+            else
+            {
+                OverMijDetailViewModel overMijDetailViewModel = new OverMijDetailViewModel()
+                {
+                    OverMijId = id,
+                    Afbeelding1 = overMij.Afbeelding1,
+                    Afbeelding2 = overMij.Afbeelding2,
+                    TekstAppInfo = overMij.TekstAppInfo,
+                    TitelAppInfo = overMij.TitelAppInfo,
+                    TekstPersInfo = overMij.TekstPersInfo,
+                    TitelPersInfo = overMij.TitelPersInfo
+                };
+                return View(overMijDetailViewModel);
+            }
         }
     }
 }
