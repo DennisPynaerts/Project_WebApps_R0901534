9373131dbcad5a8934be60b35c1644efd0959b1e
DennisPynaerts <96107261+DennisPynaerts@users.noreply.github.com>
2022-11-21 08:40:18 +0100

    Add CircuitListViewModel to CircuitView

--- Project_WebApps_R0901534_Solution/Project_WebApps_R0901534_ASP/Controllers/LeaderboardController.cs
+++ Project_WebApps_R0901534_Solution/Project_WebApps_R0901534_ASP/Controllers/LeaderboardController.cs
@@ -1,16 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Project_WebApps_R0901534_ASP.Data;
+using Project_WebApps_R0901534_ASP.Models;
+using Project_WebApps_R0901534_ASP.ViewModels;
+using System.Linq;
 
 namespace Project_WebApps_R0901534_ASP.Controllers
 {
     public class LeaderboardController : Controller
     {
+        private readonly ILogger<LeaderboardController> _logger;
+        private readonly ForzaContext _ctx;
+
+        public LeaderboardController(ILogger<LeaderboardController> logger, ForzaContext ctx)
+        {
+            _logger = logger;
+            _ctx = ctx;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var tracks = _ctx.Circuits;
+
+            CircuitListViewModel circuitListViewModel = new CircuitListViewModel();
+            circuitListViewModel.Circuits = tracks.ToList();
+
+            return View(circuitListViewModel);
         }
-        public IActionResult Detail()
+        public IActionResult Detail(int id)
         {
-            return View();
+            Circuit circuit = _ctx.Circuits.Where(c => c.CircuitId == id).FirstOrDefault();
+            if (circuit == null)
+                return RedirectToAction("Index");
+            else
+            {
+                CircuitDetailViewModel circuitDetailViewModel = new CircuitDetailViewModel()
+                {
+                    Naam = circuit.Naam
+                };
+                return View(circuitDetailViewModel);
+            }            
         }
     }
 }
