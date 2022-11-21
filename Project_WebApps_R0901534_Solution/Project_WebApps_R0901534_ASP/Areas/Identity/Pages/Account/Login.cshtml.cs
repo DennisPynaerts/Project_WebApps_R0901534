5907bde7ce1bb80089fe7303c2f567d4dcbd0182
DennisPynaerts <96107261+DennisPynaerts@users.noreply.github.com>
2022-11-21 14:33:22 +0100

    Add login functionality

--- Project_WebApps_R0901534_Solution/Project_WebApps_R0901534_ASP/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Project_WebApps_R0901534_Solution/Project_WebApps_R0901534_ASP/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -83,7 +83,7 @@ namespace Project_WebApps_R0901534_ASP.Areas.Identity.Pages.Account
             {
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.Nickname, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(Input.Nickname, Input.Password, Input.RememberMe, false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
