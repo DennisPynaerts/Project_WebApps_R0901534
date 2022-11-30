3de2ca2626a91cc643449c9a254d1941cd6e6d84
DennisPynaerts <96107261+DennisPynaerts@users.noreply.github.com>
2022-11-30 09:36:32 +0100

    Add read functionality for leaderboard

--- Project_WebApps_R0901534_Solution/Project_WebApps_R0901534_ASP/ViewModels/LaptimeListViewModel.cs
+++ Project_WebApps_R0901534_Solution/Project_WebApps_R0901534_ASP/ViewModels/LaptimeListViewModel.cs
@@ -7,12 +7,12 @@ namespace Project_WebApps_R0901534_ASP.ViewModels
     {
         public List<Laptime> Laptimes { get; set; }
         public Circuit Circuit { get; set; }
-        public AutoKlasse AutoKlasse { get; set; }
-        public GebruikerAuto GebruikerAuto { get; set; }
-        public Auto Auto { get; set; }
-        public Klasse Klasse { get; set; }
-        public Merk Merk { get; set; }
-        public Model Model { get; set; }
+        //public AutoKlasse AutoKlasse { get; set; }
+        //public GebruikerAuto GebruikerAuto { get; set; }
+        //public Auto Auto { get; set; }
+        //public Klasse Klasse { get; set; }
+        //public Merk Merk { get; set; }
+        //public Model Model { get; set; }
 
     }
 }
